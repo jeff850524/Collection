b6f4710aef198101996205e9e6cb97fc9f4e7890
jeff850524 <jeff850524@yahoo.com.tw>
2018-05-22 18:12:09 +0800

    Update Scroll

--- vendor/jquery.back-to-top.js
+++ vendor/jquery.back-to-top.js
@@ -1,17 +1,36 @@
 jQuery(document).ready(function () {
-    var offset = 220;
-    var duration = 500;
-    jQuery(window).scroll(function () {
-      if (jQuery(this).scrollTop() > offset) {
-        jQuery('.back-to-top').fadeIn(duration);
-      } else {
-        jQuery('.back-to-top').fadeOut(duration);
-      }
-    });
-
-    jQuery('.back-to-top').click(function (event) {
-      event.preventDefault();
-      jQuery('html, body').animate({ scrollTop: 0 }, duration);
-      return false;
-    })
+  var offset = 220;
+  var duration = 500;
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > offset) {
+      jQuery('.back-to-top').fadeIn(duration);
+    } else {
+      jQuery('.back-to-top').fadeOut(duration);
+    }
   });
+
+  jQuery('.back-to-top').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 0 }, duration);
+    return false;
+  })
+
+  jQuery('#ClickHome').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 0 }, duration);
+    return false;
+  })
+
+  jQuery('#ClickWork').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 480 }, duration);
+    return false;
+  })
+  jQuery('#ClickContact').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 1500 }, duration);
+    return false;
+  })
+
+
+});
